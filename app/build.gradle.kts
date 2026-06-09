48ee72b0926a4fb5d6420dd93d0582c7cf51be21
chanderquichiz <chanderquichiz@outlook.com>
2026-06-09 15:09:31 -0700

    Revert "Merge pull request #5 from ChanderQuichiz/feature/puntos-4-5-room"
    This reverts commit fe6744c8d8ee7d86f3a6733c727e43779ada43c4, reversing
    changes made to cd76a5afb8fee2fb48eac2640797db61f6c4d4c2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 
 android {
     namespace = "com.example.networkingapisrest"
-
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -33,12 +32,10 @@ android {
             )
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-
     buildFeatures {
         compose = true
     }
@@ -46,7 +43,6 @@ android {
 
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
-
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
@@ -58,18 +54,14 @@ dependencies {
     implementation("androidx.room:room-runtime:2.8.0")
     implementation("androidx.room:room-ktx:2.8.0")
     testImplementation(libs.junit)
-
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
-
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
-
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
     implementation("androidx.compose.material:material-icons-extended")
